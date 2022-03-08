94abd517c2b29c924a42308ba03e4dbd224d0e66
Reza Namazi <reza.namazi@kish.sharif.edu>
2022-03-07 16:16:05 -0800

    Add src

---
+++ src/models.py
@@ -0,0 +1,34 @@
+from torch import nn
+
+class EdgeConstruction(nn.Module):
+    def __init__(self, input_dim: int, hidden_dims: list[int]):
+        # Output size is always 1: whether there should be an edge or not
+        hidden_dims.append(1)
+        
+        self.linear = nn.ModuleList([nn.Linear(input_dim, hidden_dims[0])])
+        for in_dim, out_dim in zip(hidden_dims[:-1], hidden_dims[1:]):
+            self.linear.append(nn.ReLU())
+            self.linear.append(nn.Linear(in_dim, out_dim))
+
+        self.activation = nn.Sigmoid()
+    
+    def forward(self, x):
+        for layer in self.linear:
+            x = layer(x)
+        return self.activation(x)
+
+
+class FeatureConstruction(nn.Module):
+    def __init__(self, input_dim: int, hidden_dims: list[int], output_dim: int):
+        hidden_dims.append(output_dim)
+        
+        self.linear = nn.ModuleList([nn.Linear(input_dim, hidden_dims[0])])
+        for in_dim, out_dim in zip(hidden_dims[:-1], hidden_dims[1:]):
+            self.linear.append(nn.ReLU())
+            self.linear.append(nn.Linear(in_dim, out_dim))
+
+    
+    def forward(self, x):
+        for layer in self.linear:
+            x = layer(x)
+        return x
