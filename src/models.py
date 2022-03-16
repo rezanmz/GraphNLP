c8bf83f65e9e41fd67501f25dcd7000f7991f9c7
Reza Namazi <rezanmz@ymail.com>
2022-03-16 11:53:02 +0330

    Call constructor for nn.Module

--- src/models.py
+++ src/models.py
@@ -1,11 +1,14 @@
+from typing import List
 from torch import nn
 
 class EdgeConstruction(nn.Module):
-    def __init__(self, input_dim: int, hidden_dims: list[int]):
-        # Output size is always 1: whether there should be an edge or not
+    def __init__(self, input_dim: int, hidden_dims: List[int]):
+        super().__init__()
+
+        # Output size is always 1 (model decides whether there should be an edge or not)
         hidden_dims.append(1)
         
-        self.linear = nn.ModuleList([nn.Linear(input_dim, hidden_dims[0])])
+        self.linear = nn.ModuleList([nn.Linear(input_dim * 2, hidden_dims[0])])
         for in_dim, out_dim in zip(hidden_dims[:-1], hidden_dims[1:]):
             self.linear.append(nn.ReLU())
             self.linear.append(nn.Linear(in_dim, out_dim))
@@ -19,7 +22,9 @@ class EdgeConstruction(nn.Module):
 
 
 class FeatureConstruction(nn.Module):
-    def __init__(self, input_dim: int, hidden_dims: list[int], output_dim: int):
+    def __init__(self, input_dim: int, hidden_dims: List[int], output_dim: int):
+        super().__init__()
+
         hidden_dims.append(output_dim)
         
         self.linear = nn.ModuleList([nn.Linear(input_dim, hidden_dims[0])])
