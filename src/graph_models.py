e238c44a7528fd2f68e81d909756026a04c87336
Reza Namazi <rezanmz@ymail.com>
2022-03-16 14:35:13 +0330

    Rename to graph_models

--- src/graph_models.py
+++ src/graph_models.py
@@ -1,5 +1,6 @@
 from typing import List
 from torch import nn
+from torch_geometric.nn.dense import DenseGCNConv
 
 class EdgeConstruction(nn.Module):
     def __init__(self, input_dim: int, hidden_dims: List[int]):
@@ -37,3 +38,25 @@ class FeatureConstruction(nn.Module):
         for layer in self.linear:
             x = layer(x)
         return x
+
+
+class GCN(nn.Module):
+    def __init__(self, input_dim: int, hidden_dims: List[int], output_dim: int):
+        super().__init__()
+
+        hidden_dims.append(output_dim)
+        
+        self.gcn_conv = nn.ModuleList([DenseGCNConv(input_dim, hidden_dims[0])])
+        for in_dim, out_dim in zip(hidden_dims[:-1], hidden_dims[1:]):
+            self.gcn_conv.append(DenseGCNConv(in_dim, out_dim))
+        
+        self.activation = nn.ReLU()
+    
+    def forward(self, adj, features):
+        num_nodes = features.shape[0]
+        adj = adj.view(num_nodes, num_nodes) 
+        for layer in self.gcn_conv[:-1]:
+            features = layer(features, adj)
+            features = self.activation(features)
+        features = self.gcn_conv[-1](features, adj)
+        return features
